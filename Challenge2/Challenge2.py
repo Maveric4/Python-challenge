b679836a3c321ab87531cacbcd80830c6988a311
Daniel Gacek <maveriok@student.agh.edu.pl>
2019-08-28 19:54:26 +0200

    challenge 2 improved

--- Challenge2/Challenge2.py
+++ Challenge2/Challenge2.py
@@ -1,17 +1,17 @@
+import string
 
 text = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
-
 diff = ord('q') - ord('o')
 print(diff)
 new_text = ""
 for letter in text:
-    if letter is " ":
+    if letter == " ":
         new_letter = " "
-    elif letter is 'y':
+    elif letter == 'y':
         new_letter = 'a'
-    elif letter is 'z':
+    elif letter == 'z':
         new_letter = 'b'
-    elif letter == '.' or letter == '\'' or letter == '(' or letter == ')':
+    elif letter in string.punctuation:
         new_letter = letter
     else:
         new_letter = chr(ord(letter) + diff)
@@ -19,12 +19,11 @@ for letter in text:
 
 print(new_text)
 
-
-# print(text.maketrans())
-intab = "abcdefghijklmnopqrstuvwxyz"
+# Solving puzzle
+intab = string.ascii_lowercase
 outtab = "cdefghijklmnopqrstuvwxyzab"
 trantab = "".maketrans(intab, outtab)
-
 print("map".translate(trantab))
+
 
 
